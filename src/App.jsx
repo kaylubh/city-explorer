ec2a12693b424b42da96cc977a2fc4fd715c0edb
Caleb Hemphill <calebhemp@gmail.com>
2023-12-04 18:40:20 -0500

    Add API request for lat/long data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 // css
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// axios
+import axios from 'axios';
 // react
-
+import { useState } from 'react';
 // bootstrap
 import Container from 'react-bootstrap/Container';
 // components
 import CityInput from './components/CityInput';
 
+// API keys
+const LOC_API_KEY = import.meta.env.VITE_LOC_API_KEY;
+
 function App() {
 
-  function getLatLong(cityName) {
-    alert(cityName);
+  const [location, setLocation] = useState({});
+
+  async function getLatLong(cityName) {
+    const API_request = `https://us1.locationiq.com/v1/search.php?key=${LOC_API_KEY}&q=${cityName}&format=json`;
+    const response = await axios.get(API_request);
+    const locationObj = response.data[0];
+    setLocation(locationObj);
+    console.log(locationObj);
   }
 
   return (
